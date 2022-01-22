85b93022518a3b5e5b61684b286086f6ff2f5683
etiosa obasuyi <obasuyietiosa@gmail.com>
2022-01-22 11:42:04 -0500

    auth repo

---
+++ repositories/auth_repositories.dart
@@ -0,0 +1,20 @@
+import "package:http/http.dart";
+
+// ignore: non_constant_identifier_names
+Future<void> signIn(String? Username, String? password) async {
+  const String baseUrl = "auth/login";
+
+
+
+
+}
+//example of useing Https
+
+/* Future<void> getPost() async{
+
+  const String baseurl = "https://jsonplaceholder.typicode.com/posts";
+
+  
+
+}
+ */
