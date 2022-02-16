99b4b1bd27a1daacec70c30ceeb4070b61626c98
etiosa obasuyi <obasuyietiosa@gmail.com>
2022-02-15 19:29:18 -0500

    checkin page

--- lib/pages/appointment/registration/Checkin.dart
+++ lib/pages/appointment/registration/Checkin.dart
@@ -23,8 +23,68 @@ class _CheckinState extends State<Checkin> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.max,
         children: [
+          const Padding(
+            padding: EdgeInsets.only(top:20.0, left:20, bottom: 20),
+            child: Text('CheckIn', style:  TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+          ),
+             const Padding(
+            padding: EdgeInsets.only(left:20.0, top:10),
+            child: Text("Search",
+                style: TextStyle(color: Color.fromARGB(255, 56, 155, 152))),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left:20.0, right:10, bottom: 20),
+            child: Container(
+              constraints: const BoxConstraints(maxWidth: 350, maxHeight: 40),
+              child: TextFormField(
+                /*   onChanged: (searchParams) => context
+                          .read<AppointmentCubit>()
+                          .setSearchParams(searchParams), */
+                obscureText: false,
+                autofocus: true,
+                decoration: const InputDecoration(
+                  filled: true,
+                  fillColor: Color.fromARGB(255, 205, 226, 226),
+                  border: OutlineInputBorder(borderSide: BorderSide.none),
+                ),
+                validator: (String? value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a valid password';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Container(
+              height: 50,
+              width: 100,
+              margin: const EdgeInsets.only(left: 15),
+              constraints: const BoxConstraints(maxWidth: 170),
+              child: ElevatedButton(
+                autofocus: true,
+                style: ButtonStyle(
+                  elevation: MaterialStateProperty.all(0),
+                  backgroundColor: MaterialStateProperty.all(
+                      const Color.fromARGB(255, 56, 155, 152)),
+                ),
+                child: const Text(
+                  'Search',
+                  style: TextStyle(fontSize: 12),
+                ),
+                onPressed: () {
+                  //context.read<AppointmentCubit>().searchAppointments();
+                },
+              ),
+            ),
+          ),
           registrationBody(),
         ],
       ),
@@ -52,14 +112,14 @@ class _AppointmentState extends State<Appointment> {
   Widget build(BuildContext context) {
     return BlocBuilder<RegistrationCubit, RegistrationState>(
       builder: (context, state) {
-        print(state.appointmentList.length);
         if (state.appointmentList.isEmpty) {
           return Container(
               width: MediaQuery.of(context).size.width / 1.2,
-              height: MediaQuery.of(context).size.height / 1.2,
+              height: MediaQuery.of(context).size.height / 1.5,
               padding:
-                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0),
+                  const EdgeInsets.symmetric(horizontal: 1.0, vertical: 1.0),
               child: Column(mainAxisSize: MainAxisSize.max, children: [
+                 
                 Expanded(
                     child: Shimmer.fromColors(
                         baseColor: Colors.grey.shade300,
@@ -68,7 +128,7 @@ class _AppointmentState extends State<Appointment> {
                         child: ListView.builder(
                           shrinkWrap: true,
                           itemBuilder: (_, __) => Padding(
-                            padding: const EdgeInsets.only(bottom: 8.0),
+                            padding: const EdgeInsets.only(bottom: 1.0),
                             child: Row(
                               children: [
                                 const SizedBox(
@@ -76,7 +136,7 @@ class _AppointmentState extends State<Appointment> {
                                   height: 48,
                                 ),
                                 const Padding(
-                                  padding: EdgeInsets.only(bottom: 8.0),
+                                  padding: EdgeInsets.only(bottom: 1.0),
                                 ),
                                 Expanded(
                                     child: Column(
@@ -84,16 +144,17 @@ class _AppointmentState extends State<Appointment> {
                                   children: [
                                     Container(
                                       width: double.infinity,
-                                      height: 8.0,
+                                      height: 10.0,
                                       color: Colors.white,
-                                    ),
-                                    const Padding(
-                                      padding:
-                                          EdgeInsets.symmetric(vertical: 2.0),
                                     ),
                                     Container(
                                       width: double.infinity,
-                                      height: 8.0,
+                                      height: 10.0,
+                                      color: Colors.white,
+                                    ),
+                                       Container(
+                                      width: double.infinity,
+                                      height: 10.0,
                                       color: Colors.white,
                                     ),
                                   ],
@@ -107,82 +168,86 @@ class _AppointmentState extends State<Appointment> {
 
           return 
 
-                ListView.builder(
-              shrinkWrap: true,
-              padding: const EdgeInsets.only(top: 10, bottom: 10),
-              itemCount: state.appointmentList.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: 140,
-                    width: 200,
-                    decoration: BoxDecoration(
-                       color: const Color.fromARGB(255, 248, 254, 254),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.grey.withOpacity(0.5),
-                          spreadRadius: 1,
-                          blurRadius: 2,
-                          offset: const Offset(0, 3), // changes position of shadow
-                        ),
-                      ],
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.only(top: 10.0, left: 10),
-                          child: Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text(state.appointmentList[index]
-                                    ['firstName'], style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, ),),
-                              ),
-                              Text(state.appointmentList[index]
-                                  ['lastName'],
-                                style: const TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ],
-                            
+                Expanded(
+                  flex: 1,
+                  child: ListView.builder(
+                              shrinkWrap: true,
+                              
+                              padding: const EdgeInsets.only(top: 10, bottom: 10),
+                              itemCount: state.appointmentList.length,
+                              itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      height: 140,
+                      width: 200,
+                      decoration: BoxDecoration(
+                         color: const Color.fromARGB(255, 248, 254, 254),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 1,
+                            blurRadius: 2,
+                            offset: const Offset(0, 3), // changes position of shadow
                           ),
-                         ),
-                      const Padding(
-                        padding: EdgeInsets.only(left:15.0, top:10),
-                        child: Text('Registration date', style: TextStyle(fontSize: 12),),
+                        ],
                       ),
-                      
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: SizedBox(
-                            width: 120,
-                            height: 30,
-                            child: ElevatedButton(
-                                style: ButtonStyle(
-                                    elevation: MaterialStateProperty.all(0),
-                                    backgroundColor: MaterialStateProperty.all(
-                                        const Color.fromARGB(255, 56, 155, 152)),
-                                    shape: MaterialStateProperty.all<
-                                            RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(5.0),
-                                    ))),
-                                onPressed: () {
-                                  print("reschedule");
-                                },
-                                child: const Text('Checkin')),
-                          ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 10.0, left: 10),
+                            child: Row(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(state.appointmentList[index]
+                                      ['firstName'], style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20, ),),
+                                ),
+                                Text(state.appointmentList[index]
+                                    ['lastName'],
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
+                              
+                            ),
+                           ),
+                        const Padding(
+                          padding: EdgeInsets.only(left:15.0, top:10),
+                          child: Text('Registration date', style: TextStyle(fontSize: 12),),
                         ),
-                      ],
+                        
+                          Padding(
+                            padding: const EdgeInsets.all(10.0),
+                            child: SizedBox(
+                              width: 120,
+                              height: 30,
+                              child: ElevatedButton(
+                                  style: ButtonStyle(
+                                      elevation: MaterialStateProperty.all(0),
+                                      backgroundColor: MaterialStateProperty.all(
+                                          const Color.fromARGB(255, 56, 155, 152)),
+                                      shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(5.0),
+                                      ))),
+                                  onPressed: () {
+                                    print("reschedule");
+                                  },
+                                  child: const Text('Checkin')),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              }) 
+                  );
+                              }),
+                ) 
               ;
         }
       },
