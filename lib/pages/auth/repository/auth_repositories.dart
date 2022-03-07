39f7192605a5b6ca7bc11a515b470a7d211912eb
etiosa obasuyi <obasuyietiosa@gmail.com>
2022-03-07 15:05:05 -0500

    merge

--- lib/pages/auth/repository/auth_repositories.dart
+++ lib/pages/auth/repository/auth_repositories.dart
@@ -7,18 +7,10 @@ class AuthRepository {
   static const String contentType = "application/x-www-form-urlencoded";
   static const String accept = "application/json";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
+    // ignore: prefer_typing_uninitialized_variables
   Future<http.Response?> signIn({String? username, String? password}) async {
     // ignore: prefer_typing_uninitialized_variables
-=======
-  Future<String> signIn({String? username, String? password}) async {
-    dynamic body;
->>>>>>> 6818e5536e212308001e95b84db12686c985d199
-=======
-  Future<http.Response?> signIn({String? username, String? password}) async {
-    // ignore: prefer_typing_uninitialized_variables
->>>>>>> c21b91e78bb312ca89d6cba091fc1a4ad2b57e2f
     try {
       var uri = Uri.parse(root + "auth/login");
       http.Response response = await http.post(uri,
@@ -28,31 +20,17 @@ class AuthRepository {
           },
           encoding: Encoding.getByName("utf-8"),
           body: {"username": username, "password": password});
-<<<<<<< HEAD
-<<<<<<< HEAD
-      return reponse;
-=======
-      body = jsonDecode(response.body);
-      return body['token'];
->>>>>>> 6818e5536e212308001e95b84db12686c985d199
-=======
-      return response;
->>>>>>> c21b91e78bb312ca89d6cba091fc1a4ad2b57e2f
-    } catch (e) {
+        return response;
+
+    } catch(e){
       return null;
     }
   }
 
   Future<http.Response?> home({String? token}) async {
     var uri = Uri.parse(root + "api/rest/v1/home");
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
     dynamic body;
->>>>>>> 6818e5536e212308001e95b84db12686c985d199
-=======
-    dynamic body;
->>>>>>> c21b91e78bb312ca89d6cba091fc1a4ad2b57e2f
     try {
       http.Response response = await http.get(uri, headers: {
         "Content-Type": "application/json",
@@ -85,4 +63,4 @@ class AuthRepository {
       return body;
     }
   }
-}
+  }
