5dbd02a5d684e6efa82d69615cc12a4c7a03aeb6
Etiosa Obasuyi <obasuyietiosa@gmail.com>
2022-06-02 15:54:42 -0400

    appointment analytics implemented

---
+++ lib/system/analytics/appointment_analytics.dart
@@ -0,0 +1,110 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+
+import 'event.dart';
+
+class AppointmentAnalytics {
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+
+  void createLog(
+      {required String userId,
+      required String appointmentId,
+      required String timeCreation,
+      required String firstName,
+      required String lastName,
+      required String userType}) async {
+    setUserLog(
+        firstName: firstName,
+        lastName: lastName,
+        userId: userId,
+        userType: userType);
+
+/*     await analytics.logEvent(name: EVENTTYPE.CREATE.toString(), parameters: {
+      "userId": userId,
+      "userType": userType,
+      "timeCreation": timeCreation,
+      "appointmentId": appointmentId
+    }); */
+    logEevent(
+        appointmentId: appointmentId,
+        userId: userId,
+        time: timeCreation,
+        userType: userType);
+  }
+
+  void cancelLog(
+      {required String userId,
+      required String appointmentId,
+      required String timeDeletion,
+      required String firstName,
+      required String lastName,
+      required String userType}) async {
+    setUserLog(
+        firstName: firstName,
+        lastName: lastName,
+        userId: userId,
+        userType: userType);
+    /*    await analytics.logEvent(name: EVENTTYPE.CANCEL.toString(), parameters: {
+      "userId": userId,
+      "userType": userType,
+      "timeDeletion": timeDeletion,
+      "appointmentId": appointmentId
+    });
+ */
+    logEevent(
+        appointmentId: appointmentId,
+        userId: userId,
+        time: timeDeletion,
+        userType: userType);
+  }
+  //loginedUser
+  //medical user information
+
+  void editLog(
+      {required String userId,
+      required String appointmentId,
+      required String timeEdit,
+      required String firstName,
+      required String lastName,
+      required String userType}) async {
+    setUserLog(
+        firstName: firstName,
+        lastName: lastName,
+        userId: userId,
+        userType: userType);
+    /* await analytics.logEvent(name: EVENTTYPE.EDIT.toString(), parameters: {
+      "userId": userId,
+      "userType": userType,
+      "timeEdit": timeEdit,
+      "appointmentId": appointmentId
+    }); */
+    logEevent(
+        appointmentId: appointmentId,
+        userId: userId,
+        time: timeEdit,
+        userType: userType);
+  }
+
+  void setUserLog(
+      {required String firstName,
+      required String lastName,
+      required String userId,
+      required String userType}) async {
+    await analytics.setUserId(id: userId);
+    await analytics.setUserProperty(name: "firstName", value: firstName);
+    await analytics.setUserProperty(name: "lastName", value: lastName);
+    await analytics.setUserProperty(name: "userType", value: userType);
+  }
+
+  void logEevent(
+      {required String userId,
+      required String userType,
+      required String time,
+      required String appointmentId}) async {
+    await analytics.logEvent(name: EVENTTYPE.EDIT.toString(), parameters: {
+      "userId": userId,
+      "userType": userType,
+      "time": time,
+      "appointmentId": appointmentId
+    });
+  }
+}
